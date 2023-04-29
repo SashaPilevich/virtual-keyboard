832135a467ea8262a67e0f3e69af25f07feb492f
Sasha <pilevichsasha@gmail.com>
2023-04-29 19:10:00 +0300

    feat:add event listener for keyup and keydown for all button, include special case for Capslock/Shift/Enter/Delete/Backspace/Space/Alt/Arrows and hotKeys

--- src/keyboard.js
+++ src/keyboard.js
@@ -6,11 +6,11 @@ const keyboardFragment = document.createElement("div");
 keyboardFragment.classList.add("keyboard-container");
 
 export class Keyboard {
-  constructor(){
+  constructor() {
     this.lang = localStorage.getItem("language") === "ru" ? "ru" : "en";
     this.capsLock = false;
   }
-  init(){
+  init() {
     this.textarea = document.createElement("textarea");
     this.textarea.classList.add("textarea-keyboard");
     this.textarea.setAttribute("rows", 10);
@@ -30,6 +30,95 @@ export class Keyboard {
       }
       keyboardFragment.append(keyboardRow);
     }
-    initKeyboardPage(this.textarea,keyboardFragment)
+    initKeyboardPage(this.textarea, keyboardFragment);
+    this._clickPhysicalKey();
+    this._changeLanguage();
   }
+
+  _clickPhysicalKey() {
+    document.addEventListener("keydown", (event) => {
+      event.stopImmediatePropagation();
+      let key = document.getElementById(event.code);
+      if (!key) {
+        event.preventDefault();
+        return;
+      }
+      if (event.code === "CapsLock" && !event.repeat) {
+        this.capsLock = !this.capsLock;
+        if (this.capsLock) {
+          key.classList.add("active");
+        } else {
+          key.classList.remove("active");
+        }
+        this._clickWithShift(event.shiftKey);
+      } else {
+        key.classList.add("active");
+        if (event.code === "Backspace") {
+          event.preventDefault();
+          let pointForStart = Math.max(0, this.textarea.selectionStart - 1);
+          this.textarea.value =
+            this.textarea.value.slice(0, pointForStart) +
+            this.textarea.value.slice(this.textarea.selectionEnd);
+          this.textarea.selectionStart = pointForStart;
+          this.textarea.selectionEnd = this.textarea.selectionStart;
+        } else if (event.code === "Tab") {
+          event.preventDefault();
+          this.textarea.value =
+            this.textarea.value.slice(0, this.textarea.selectionStart) +
+            "\t" +
+            this.textarea.value.slice(this.textarea.selectionEnd);
+          this.textarea.selectionStart =
+            this.textarea.selectionStart + "\t".length;
+          this.textarea.selectionEnd = this.textarea.selectionStart;
+        } else if (event.code === "Delete") {
+          event.preventDefault();
+          let pointForEnd = this.textarea.selectionEnd;
+          if (pointForEnd < this.textarea.value.length) {
+            let arrayOfValue = this.textarea.value.split("");
+            arrayOfValue.splice(pointForEnd, 1);
+            this.textarea.value = arrayOfValue.join("");
+            this.textarea.selectionEnd = pointForEnd;
+          }
+        } else if (event.code === "Enter") {
+          event.preventDefault();
+          this.textarea.value += "\n";
+        } else if (event.code === "Space") {
+          event.preventDefault();
+          this.textarea.value += " ";
+        } else if (event.altKey && event.shiftKey && !event.repeat) {
+          event.preventDefault();
+          this.lang = this.lang === "ru" ? "en" : "ru";
+          localStorage.setItem("language", this.lang);
+          this._changeLanguage(this.lang);
+        } else if (
+          (event.code === "ShiftLeft" || event.code === "ShiftRight") &&
+          !event.repeat
+        ) {
+          event.preventDefault();
+          this._clickWithShift(event.shiftKey);
+        } else if(event.code === "ArrowUp" || event.code === "ArrowDown" || event.code === "ArrowLeft" || event.code === "ArrowRight"){
+          event.preventDefault();
+          this._clickOnArrow();
+        }
+        else if (!keyboardObj[event.code].state) {
+          this.textarea.value += key.textContent;
+        }
+      }
+    });
+
+    document.addEventListener("keyup", (event) => {
+      event.stopImmediatePropagation();
+      let key = document.getElementById(event.code);
+      if (event.code !== "CapsLock") {
+        key.classList.remove("active");
+        if (event.code === "ShiftRight" || event.code === "ShiftLeft") {
+          event.preventDefault();
+          this._clickWithShift(event.shiftKey);
+        }
+      }
+    });
+  }
+  _changeLanguage(){}
+  _clickWithShift(shiftKey) {}
+  _clickOnArrow(){}
 }
